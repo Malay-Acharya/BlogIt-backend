14ad0d72b0f5f547a21484a22e76b3c4b2a50bea
Malay <malayacharya21@gmail.com>
2022-01-18 05:17:56 +0530

    Added the error boundaries to signup and login routes

--- index.js
+++ index.js
@@ -11,8 +11,31 @@ app.use(express.json())
 
 mongoose.connect('mongodb://localhost:27017/blogit')
 
+
 app.post('/api/register', async (req, res) => {
     try{
+        const emailRegexp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+        if(!emailRegexp.test(req.body.email)){
+            res.json({status:'Enter a valid mail'})
+            return;
+        }
+        const user = await User.findOne({
+            email: req.body.email,
+        })
+        if(user){
+            res.json({status:'Email already in use'})
+        }
+        else{
+        if(!req.body.name){
+            res.json({status:'Enter name'})
+        }
+        else if(!req.body.email){
+            res.json({status:'Enter email'})
+        }
+        else if(!req.body.password){
+            res.json({status:'Enter password'})
+        }
+        else{
         const user = await User.create({
             name: req.body.name,
             email: req.body.email,
@@ -20,6 +43,8 @@ app.post('/api/register', async (req, res) => {
         })
         console.log(user);
         res.json({status: 'ok'})
+    }
+    }
     }catch(err){
         res.json({status: err})
     }
@@ -39,7 +64,7 @@ app.post('/api/login', async (req, res) => {
 
             return res.json({status:'ok', user:token})
         }else{
-            return res.json({status:'error'})
+            return res.json({status:'Wrong credentials'})
         }
     }catch(err){
         console.log(err);
